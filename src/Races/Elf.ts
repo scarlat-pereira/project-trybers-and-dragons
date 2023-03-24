87cae06962c1db7dc11188a63d92f84c6f90874a
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-03-24 16:24:43 -0300

    feat: add req02 - created Dwarf, Elf, Halfling and Orc classes

---
+++ src/Races/Elf.ts
@@ -0,0 +1,21 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private static _numberInstances = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    Elf._numberInstances += 1;
+    this._maxLifePoints = 99;
+  }
+
+  static createdRacesInstances(): number {
+    return this._numberInstances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
---
+++ src/Races/Orc.ts
@@ -0,0 +1,21 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  private static _numberInstances = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+
+    Orc._numberInstances += 1;
+    this._maxLifePoints = 74;
+  }
+
+  static createdRacesInstances(): number {
+    return this._numberInstances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
